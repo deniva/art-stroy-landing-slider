729ed263bee15152643413f3c39eb92cbde99883
Denis Ivanov <denis.ivanov@mail.ru>
2023-04-23 14:48:15 +0300

    update

--- app/js/app.js
+++ app/js/app.js
@@ -108,47 +108,47 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	// Cursor
 
-	const body   = document.querySelector('body'),
-				cursor = document.getElementById('cursor'),
-				links  = document.getElementsByTagName('a')
+	// const body   = document.querySelector('body'),
+	// 			cursor = document.getElementById('cursor'),
+	// 			links  = document.getElementsByTagName('a')
 
-	let mouseX = 0, mouseY = 0, posX = 0, posY = 0
+	// let mouseX = 0, mouseY = 0, posX = 0, posY = 0
 
-	function mouseCoords(e) {
-		mouseX = e.pageX
-		mouseY = e.pageY
-	}
+	// function mouseCoords(e) {
+	// 	mouseX = e.pageX
+	// 	mouseY = e.pageY
+	// }
 
-	gsap.to({}, .01, {
-		repeat: -1,
-		onRepeat: () => {
-			posX += (mouseX - posX) / 6
-			posY += (mouseY - posY) / 6
-			gsap.set(cursor, {
-				css: {
-					left: posX,
-					top: posY
-				}
-			})
-		}
-	})
+	// gsap.to({}, .01, {
+	// 	repeat: -1,
+	// 	onRepeat: () => {
+	// 		posX += (mouseX - posX) / 6
+	// 		posY += (mouseY - posY) / 6
+	// 		gsap.set(cursor, {
+	// 			css: {
+	// 				left: posX,
+	// 				top: posY
+	// 			}
+	// 		})
+	// 	}
+	// })
 
-	for(let i = 0; i < links.length; i++) {
-		links[i].addEventListener('mouseover', () => {
-			cursor.classList.add('active')
-		})
-		links[i].addEventListener('mouseout', () => {
-			cursor.classList.remove('active')
-		})
-	}
+	// for(let i = 0; i < links.length; i++) {
+	// 	links[i].addEventListener('mouseover', () => {
+	// 		cursor.classList.add('active')
+	// 	})
+	// 	links[i].addEventListener('mouseout', () => {
+	// 		cursor.classList.remove('active')
+	// 	})
+	// }
 
-	body.addEventListener('mousemove', e => {
-		mouseCoords(e)
-		cursor.classList.remove('hidden')
-	})
+	// body.addEventListener('mousemove', e => {
+	// 	mouseCoords(e)
+	// 	cursor.classList.remove('hidden')
+	// })
 
-	body.addEventListener('mouseout', e => {
-		cursor.classList.add('hidden')
-	})
+	// body.addEventListener('mouseout', e => {
+	// 	cursor.classList.add('hidden')
+	// })
 
 })
